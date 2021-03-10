cf5c0263e7a982f75c2c172dc4eb9e951a12581a
MustafaBeyatli <mustafaorhan754@gmail.com>
2021-03-10 18:27:36 +0300

    version 1.02

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let user = {
     firstName: "Mustafa",
     lastName: "Beyatli",
+
+    getFullName: function () {
+        return `Full Name : ${ this.firstName } ${ this.lastName }`;
+    }
 };
 
 console.log(user.firstName);
 console.log(user.lastName);
+console.log(user.getFullName());
